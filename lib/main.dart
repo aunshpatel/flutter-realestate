a2041d1ce98999b9bf2162b113db1bd8633a396c
aunshpatel <patelaunsh28@gmail.com>
2024-07-26 23:23:25 -0400

    Code cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -82,26 +82,3 @@ class _MyAppState extends State<MyApp> {
 // Platform  Firebase App Id
 // android   1:847318552692:android:a1e40d72039ba03355c121
 // ios       1:847318552692:ios:f80b17094ea0039a55c121
-
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'RealEstate',
-//       theme: ThemeData(
-//         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(0, 227, 231, 239)),
-//         useMaterial3: false,
-//       ),
-//       initialRoute: '/main_screen',
-//       routes: {
-//         '/main_screen':(context) => const MainScreen(),
-//         '/login_screen':(context) => const LoginScreen(),
-//         '/profile_page':(context) => const MyProfile(),
-//       },
-//     );
-//   }
-// }
